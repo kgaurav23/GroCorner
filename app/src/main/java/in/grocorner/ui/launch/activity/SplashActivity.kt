2dffe013ee16fc751af6d3af09ad978e064a4367
kumar.sumit <kumar.sumit@redbus.com>
2020-08-25 18:44:18 +0530

    Added position to fragment

--- app/src/main/java/in/grocorner/ui/launch/activity/SplashActivity.kt
+++ app/src/main/java/in/grocorner/ui/launch/activity/SplashActivity.kt
@@ -2,6 +2,7 @@ package `in`.grocorner.ui.launch.activity
 
 import `in`.grocorner.R
 import `in`.grocorner.ui.launch.contract.SplashScreenContract
+import `in`.grocorner.ui.launch.navigator.LaunchScreenNavigator
 import `in`.grocorner.ui.launch.presenter.SplashActivityPresenter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
